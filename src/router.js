55de93e1730bccc8156f228625b725ca572e62ee
Daltonic <darlingtongospel@gmail.com>
2021-02-09 14:55:51 +0100

    Feat: Installs firebase npm package and configuration

--- src/router.js
+++ src/router.js
@@ -9,31 +9,29 @@ export default new Router({
       path: '/',
       component: () => import('./views/Projects.vue'),
       name: 'projects',
-      meta: { requireAuth: true }
+      meta: { requiresAuth: true }
     },
     {
       path: '/login',
       component: () => import('./views/Login.vue'),
       name: 'login',
-      meta: { requireAuth: true }
     },
     {
       path: '/signup',
       component: () => import('./views/Register.vue'),
       name: 'signup',
-      meta: { requireAuth: true }
     },
     {
       path: '/mockups',
       component: () => import('./views/MockUps.vue'),
       name: 'mockups',
-      meta: { requireAuth: true }
+      meta: { requiresAuth: true }
     },
     {
       path: '/projects/:id',
       component: () => import('./views/Project.vue'),
       name: 'project',
-      meta: { requireAuth: true }
+      meta: { requiresAuth: true }
     },
   ],
   mode: 'history'
